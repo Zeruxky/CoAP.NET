64bbef4bdd45ad77df039f0c02d63bf4c10393a7
Philip Wille <philip.wille@world-direct.at>
2020-10-16 13:22:04 +0200

    Added Client and Server example

--- WorldDirect.CoAP.Example.Server/Program.cs
+++ WorldDirect.CoAP.Example.Server/Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
+using WorldDirect.CoAP.Server;
 
 namespace WorldDirect.CoAP.Example.Server
 {
+    using CoAP.Server.Resources;
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var server = new CoapServer();
+            server.Add(new HelloWorldResource("HelloWorld"));
+            server.Start();
+            Console.ReadLine();
+        }
+    }
+
+    public class HelloWorldResource : Resource
+    {
+        /// <inheritdoc />
+        public HelloWorldResource(string name) : base(name)
+        {
+            Attributes.Title = "GET a friendly greeting!";
+            Attributes.AddResourceType("HelloWorldDisplayer");
+        }
+
+        /// <inheritdoc />
+        public HelloWorldResource(string name, bool visible) : base(name, visible)
+        {
+        }
+
+        /// <inheritdoc />
+        protected override void DoGet(CoapExchange exchange)
+        {
+            exchange.Respond(StatusCode.Content, "Hello World!");
         }
     }
 }
